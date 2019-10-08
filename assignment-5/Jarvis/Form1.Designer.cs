aacde631a405362c220ff4534b941298778b40ae
= <=>
2019-10-08 20:05:32 +0300

    Реализован алгоритм Джарвиса

---
+++ assignment-5/Jarvis/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Jarvis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(659, 536);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(114, 555);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Draw convex hull";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.DrawConvexHull_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(431, 555);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 588);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ assignment-5/Jarvis/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jarvis
+{
+    public partial class Form1 : Form
+    {
+        Graphics graphics;
+        List<Point> points;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Image image = new Bitmap(pictureBox.Width, pictureBox.Height);
+            graphics = Graphics.FromImage(image);
+            pictureBox.Image = image;
+            points = new List<Point>();
+        }
+
+        private void DrawConvexHull_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.Transparent);
+            pictureBox.Refresh();
+            foreach (Point p in points)
+                DrawCrossPoint(p.X, p.Y);
+
+            List<Point> hull = JarvisAlgorithm();
+            for (int i = 1; i < hull.Count; i++)
+            {
+                graphics.DrawLine(Pens.Black, hull[i - 1], hull[i]);
+                //pictureBox.Refresh();
+                //System.Threading.Thread.Sleep(500);
+            }
+            graphics.DrawLine(Pens.Black, hull[0], hull.Last());
+            pictureBox.Refresh();
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.Transparent);
+            pictureBox.Refresh();
+            points.Clear();
+        }
+
+        private void PictureBox_Click(object sender, MouseEventArgs e)
+        {
+            AddPoint(e.Location.X, e.Location.Y);
+        }
+
+        private void AddPoint(int x, int y)
+        {
+            Point point = new Point(x, y);
+            points.Add(point);
+            DrawCrossPoint(x, y);
+            pictureBox.Refresh();
+        }
+
+        static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+
+        private void DrawPoint(int x, int y) { graphics.FillRectangle(Brushes.Black, x, y, 1, 1); }
+
+        private void DrawCrossPoint(int x, int y)
+        {
+            graphics.FillRectangle(Brushes.Black, x, y, 1, 1);
+            graphics.FillRectangle(Brushes.Black, x - 1, y, 1, 1);
+            graphics.FillRectangle(Brushes.Black, x, y - 1, 1, 1);
+            graphics.FillRectangle(Brushes.Black, x + 1, y, 1, 1);
+            graphics.FillRectangle(Brushes.Black, x, y + 1, 1, 1);
+        }
+
+
+        public List<Point> JarvisAlgorithm()
+        {
+            List<Point> hull = new List<Point>();
+
+            Point vPointOnHull = points.Where(p => p.X == points.Min(min => min.X)).First();
+
+            Point vEndPoint;
+            do
+            {
+                hull.Add(vPointOnHull);
+                vEndPoint = points[0];
+
+                for (int i = 1; i < points.Count; i++)
+                {
+                    if ((vPointOnHull == vEndPoint) || rotate(vPointOnHull, vEndPoint, points[i]))
+                        vEndPoint = points[i];
+                }
+
+                vPointOnHull = vEndPoint;
+            }
+            while (vEndPoint != hull[0]);
+
+            return hull;
+        }
+
+        // Возвращает truе если С находится слева от АВ, false если справа.
+        private bool rotate(Point A, Point B, Point C)
+        {
+            return (B.X - A.X) * (C.Y - B.Y) - (B.Y - A.Y) * (C.X - B.X) > 0;
+        }
+    }
+}
